db3288022d178f256cf88cccfaea7acc002ee57b
Jeromy Wendrick <wendrickje@gmail.com>
2016-06-23 19:30:19 -0400

    farseer source to vendor

---
+++ Vendor/Source/Farseer/FarseerPhysics/DebugViewBase.cs
@@ -0,0 +1,214 @@
+﻿/*
+* Farseer Physics Engine:
+* Copyright (c) 2012 Ian Qvist
+*/
+
+using System;
+using FarseerPhysics.Common;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using FarseerPhysics.Dynamics.Contacts;
+using FarseerPhysics.Collision;
+
+namespace FarseerPhysics
+{
+    [Flags]
+    public enum DebugViewFlags
+    {
+        /// <summary>
+        /// Draw shapes.
+        /// </summary>
+        Shape = (1 << 0),
+
+        /// <summary>
+        /// Draw joint connections.
+        /// </summary>
+        Joint = (1 << 1),
+
+        /// <summary>
+        /// Draw axis aligned bounding boxes.
+        /// </summary>
+        AABB = (1 << 2),
+
+        /// <summary>
+        /// Draw broad-phase pairs.
+        /// </summary>
+        //Pair = (1 << 3),
+
+        /// <summary>
+        /// Draw center of mass frame.
+        /// </summary>
+        CenterOfMass = (1 << 4),
+
+        /// <summary>
+        /// Draw useful debug data such as timings and number of bodies, joints, contacts and more.
+        /// </summary>
+        DebugPanel = (1 << 5),
+
+        /// <summary>
+        /// Draw contact points between colliding bodies.
+        /// </summary>
+        ContactPoints = (1 << 6),
+
+        /// <summary>
+        /// Draw contact normals. Need ContactPoints to be enabled first.
+        /// </summary>
+        ContactNormals = (1 << 7),
+
+        /// <summary>
+        /// Draws the vertices of polygons.
+        /// </summary>
+        PolygonPoints = (1 << 8),
+
+        /// <summary>
+        /// Draws the performance graph.
+        /// </summary>
+        PerformanceGraph = (1 << 9),
+
+        /// <summary>
+        /// Draws controllers.
+        /// </summary>
+        Controllers = (1 << 10)
+    }
+
+    /// Implement and register this class with a World to provide debug drawing of physics
+    /// entities in your game.
+    public abstract class DebugViewBase
+    {
+        //Shapes
+        public Color DefaultShapeColor = new Color(0.9f, 0.7f, 0.7f);
+        public Color InactiveShapeColor = new Color(0.5f, 0.5f, 0.3f);
+        public Color KinematicShapeColor = new Color(0.5f, 0.5f, 0.9f);
+        public Color SleepingShapeColor = new Color(0.6f, 0.6f, 0.6f);
+        public Color StaticShapeColor = new Color(0.5f, 0.9f, 0.5f);
+
+        //Contacts
+        private int _pointCount;
+        private const int MaxContactPoints = 2048;
+        private Vector2[] _points = new Vector2[MaxContactPoints];
+
+        public Color TextColor = Color.White;
+        protected DebugViewBase(World world)
+        {
+            World = world;
+        }
+/*
+        public void OnPreSolve(Contact contact, ref Manifold oldManifold)
+        {
+            if ((Flags & DebugViewFlags.ContactPoints) == DebugViewFlags.ContactPoints)
+            {
+                Manifold manifold = contact.Manifold;
+
+                if (manifold.PointCount == 0)
+                    return;
+
+                Fixture fixtureA = contact.FixtureA;
+
+                FixedArray2<PointState> state1, state2;
+                Collision.Collision.GetPointStates(out state1, out state2, ref oldManifold, ref manifold);
+
+                FixedArray2<Vector2> points;
+                Vector2 normal;
+                contact.GetWorldManifold(out normal, out points);
+
+                for (int i = 0; i < manifold.PointCount && _pointCount < MaxContactPoints; ++i)
+                {
+                    if (fixtureA == null)
+                        _points[i] = new Vector2();
+
+                    Vector2 cp = _points[_pointCount];
+                    cp.Position = points[i];
+                    cp.Normal = normal;
+                    cp.State = state2[i];
+                    _points[_pointCount] = cp;
+                    ++_pointCount;
+                }
+            }
+        }*/
+
+        protected World World { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the debug view flags.
+        /// </summary>
+        /// <value>The flags.</value>
+        public DebugViewFlags Flags { get; set; }
+
+        /// <summary>
+        /// Append flags to the current flags.
+        /// </summary>
+        /// <param name="flags">The flags.</param>
+        public void AppendFlags(DebugViewFlags flags)
+        {
+            Flags |= flags;
+        }
+
+        /// <summary>
+        /// Remove flags from the current flags.
+        /// </summary>
+        /// <param name="flags">The flags.</param>
+        public void RemoveFlags(DebugViewFlags flags)
+        {
+            Flags &= ~flags;
+        }
+
+        /// <summary>
+        /// Draw a closed polygon provided in CCW order.
+        /// </summary>
+        /// <param name="vertices">The vertices.</param>
+        /// <param name="count">The vertex count.</param>
+        /// <param name="red">The red value.</param>
+        /// <param name="blue">The blue value.</param>
+        /// <param name="green">The green value.</param>
+        public abstract void DrawPolygon(Vector2[] vertices, int count, Color inColor, bool closed = true);
+
+        /// <summary>
+        /// Draw a solid closed polygon provided in CCW order.
+        /// </summary>
+        /// <param name="vertices">The vertices.</param>
+        /// <param name="count">The vertex count.</param>
+        /// <param name="red">The red value.</param>
+        /// <param name="blue">The blue value.</param>
+        /// <param name="green">The green value.</param>
+        public abstract void DrawSolidPolygon(Vector2[] vertices, int count, Color inColor);
+
+        /// <summary>
+        /// Draw a circle.
+        /// </summary>
+        /// <param name="center">The center.</param>
+        /// <param name="radius">The radius.</param>
+        /// <param name="red">The red value.</param>
+        /// <param name="blue">The blue value.</param>
+        /// <param name="green">The green value.</param>
+        public abstract void DrawCircle(Vector2 center, float radius, Color inColor);
+
+        /// <summary>
+        /// Draw a solid circle.
+        /// </summary>
+        /// <param name="center">The center.</param>
+        /// <param name="radius">The radius.</param>
+        /// <param name="axis">The axis.</param>
+        /// <param name="red">The red value.</param>
+        /// <param name="blue">The blue value.</param>
+        /// <param name="green">The green value.</param>
+        public abstract void DrawSolidCircle(Vector2 center, float radius, Vector2 axis, Color inColor);
+
+        /// <summary>
+        /// Draw a line segment.
+        /// </summary>
+        /// <param name="start">The start.</param>
+        /// <param name="end">The end.</param>
+        /// <param name="red">The red value.</param>
+        /// <param name="blue">The blue value.</param>
+        /// <param name="green">The green value.</param>
+        public abstract void DrawSegment(Vector2 start, Vector2 end, Color inColor);
+
+        public abstract void DrawPoint(Vector2 point, float radius, Color inColor);
+
+        /// <summary>
+        /// Draw a transform. Choose your own length scale.
+        /// </summary>
+        /// <param name="transform">The transform.</param>
+        public abstract void DrawTransform(ref Transform transform);
+    }
+}

ef638f198769e092a551ffc47c4a70fbc187d648
Jeromy Wendrick <wendrickje@gmail.com>
2016-06-23 21:03:33 -0400

    sync vendor assemblies with compile from source versions; removed unused assemblies for other platforms (can be added back later)

---
+++ Vendor/Source/MonoGame/Tools/Pipeline/Controls/PropertyCells/CellText.cs
@@ -0,0 +1,37 @@
+﻿// MonoGame - Copyright (C) The MonoGame Team
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+
+using System;
+using Eto.Forms;
+
+namespace MonoGame.Tools.Pipeline
+{
+    public class CellText : CellBase
+    {
+        public CellText(string category, string name, object value, EventHandler eventHandler, bool editable) : base(category, name, value, eventHandler)
+        {
+            Editable = editable && value is string;
+        }
+
+        public override void Edit(PixelLayout control)
+        {
+            var editText = new TextBox();
+            editText.Style = "OverrideSize";
+            editText.Width = _lastRec.Width;
+            editText.Height = _lastRec.Height;
+            editText.Text = Value.ToString();
+
+            control.Add(editText, _lastRec.X, _lastRec.Y);
+
+            editText.EnabledChanged += delegate
+            {
+                if (_eventHandler == null)
+                    return;
+                
+                _eventHandler(editText.Text, EventArgs.Empty);
+            };
+        }
+    }
+}
+
